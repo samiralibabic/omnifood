20aae88caa357c150bcaa7e9c87213712ece2bc6
Samir Alibabic <samir.alibabic@gmail.com>
2016-03-22 22:41:20 +0100

    Initial commit of final project

---
+++ resources/js/script.js
@@ -0,0 +1,87 @@
+$(document).ready(function() {
+
+    /* For sticky navigation */
+    $('.js--section-features').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+      offset: '60px;'
+    });
+
+    /* Scroll from start page buttons */
+    $('.js--scroll-to-plans').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    });
+    $('.js--scroll-to-start').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    });
+
+    /* Navigation smooth scroll */
+    $(function() {
+      $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
+
+    /* Animations on scrolling */
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeIn')
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated fadeInUp')
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeIn')
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated pulse')
+    }, {
+        offset: '50%'
+    });
+
+    /* Mobile navigation button click */
+    $('.js--mobile-nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--mobile-nav-icon i');
+        nav.slideToggle(200);
+        icon.toggleClass("ion-close-round ion-navicon-round");
+    })
+
+    /* Maps */
+    var map = new GMaps({
+      div: '.map',
+      lat: 44.8140000,
+      lng: 15.8800000
+    });
+
+    map.addMarker({
+      lat: 44.811520,
+      lng: 15.866416,
+      title: 'Bihac',
+      infoWindow: {
+          content: '<p>You found me!</p>'
+      }
+    });
+
+});
